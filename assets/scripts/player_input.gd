f51d4bf90357a044f85e9051f54add6d3c796ed2
Liam Wofford <27900656+nulture@users.noreply.github.com>
2025-01-18 14:53:07 -0600

    Updated move speed/damping

--- assets/scripts/player_input.gd
+++ assets/scripts/player_input.gd
@@ -4,6 +4,7 @@ extends Node
 const JUMP_VELOCITY = 4.5
 
 @export var move_speed : float = 5.0
+@export var move_damping : float = 5.0
 
 @onready var pawn : CharacterBody3D = self.get_parent()
 
@@ -13,9 +14,6 @@ func _process(delta: float) -> void:
 	move_vector = Input.get_vector("move_left", "move_right", "move_up", "move_down").normalized()
 
 func _physics_process(delta: float) -> void:
-	# Add the gravity.
-	if not pawn.is_on_floor():
-		pawn.velocity += pawn.get_gravity() * delta
 
 	# Handle jump.
 	if Input.is_action_just_pressed("ui_accept") and pawn.is_on_floor():
@@ -25,11 +23,11 @@ func _physics_process(delta: float) -> void:
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var input_dir := Input.get_vector("move_left", "move_right", "move_up", "move_down")
 	var direction := (pawn.transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
-	if direction:
-		pawn.velocity.x = direction.x * move_speed
-		pawn.velocity.z = direction.z * move_speed
-	else:
-		pawn.velocity.x = move_toward(pawn.velocity.x, 0, move_speed)
-		pawn.velocity.z = move_toward(pawn.velocity.z, 0, move_speed)
+	# if direction:
+	# 	pawn.velocity.x = direction.x * move_speed
+	# 	pawn.velocity.z = direction.z * move_speed
+
+	pawn.velocity += direction * move_speed * delta
+	pawn.velocity -= pawn.velocity * move_damping * delta
 
 	pawn.move_and_slide()

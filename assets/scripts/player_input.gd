daa87b655267f56315fb814ccd680540a221ed08
Liam Wofford <27900656+nulture@users.noreply.github.com>
2025-01-18 15:14:38 -0600

    Added dodging

--- assets/scripts/player_input.gd
+++ assets/scripts/player_input.gd
@@ -3,28 +3,47 @@ extends Node
 
 const JUMP_VELOCITY = 4.5
 
-@export var move_speed : float = 5.0
-@export var move_damping : float = 5.0
+@export_category("Walk")
+
+@export var walk_speed : float = 5.0
+@export var walk_damping : float = 5.0
+
+@export_category("Dodge")
+
+@export var dodge_impulse : float = 50.0
+@export var dodge_damping : float = 3.0
+@export var dodge_delay : float = 1.0
 
 @onready var pawn : CharacterBody3D = self.get_parent()
 
-var move_vector : Vector2
+var move_vector : Vector3
+var is_dodging : bool
+
+var damping : float :
+	get: return dodge_damping if is_dodging else walk_damping
 
 func _process(delta: float) -> void:
-	move_vector = Input.get_vector("move_left", "move_right", "move_up", "move_down").normalized()
+	var input_vector := Input.get_vector("move_left", "move_right", "move_up", "move_down").normalized()
+	move_vector = Vector3(input_vector.x, 0, input_vector.y)
 
 func _physics_process(delta: float) -> void:
+	var move_direction := (pawn.transform.basis * move_vector)
 
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and pawn.is_on_floor():
-		pawn.velocity.y = JUMP_VELOCITY
+	if not is_dodging:
+		pawn.velocity += move_direction * walk_speed * delta
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var input_dir := Input.get_vector("move_left", "move_right", "move_up", "move_down")
-	var move_direction := (pawn.transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
-
-	pawn.velocity += move_direction * move_speed * delta
-	pawn.velocity -= pawn.velocity * move_damping * delta
+	pawn.velocity -= pawn.velocity * damping * delta
 
 	pawn.move_and_slide()
+
+
+func _input(event: InputEvent) -> void:
+	if event.is_action_pressed("dodge"):
+		dodge()
+
+func dodge() -> void:
+	if is_dodging: return
+	is_dodging = true
+	pawn.velocity += move_vector * dodge_impulse
+	await get_tree().create_timer(dodge_delay).timeout
+	is_dodging = false

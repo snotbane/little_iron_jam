6b20013799937da3eb06ee11d720aeff585aac73
Liam Wofford <27900656+nulture@users.noreply.github.com>
2025-01-18 14:45:18 -0600

    Fixed aiming weirdness

--- assets/scripts/crosshair.gd
+++ assets/scripts/crosshair.gd
@@ -9,6 +9,7 @@ extends Node3D
 @export var aim_visual_alpha : float = 10.0
 
 var aim_vector : Vector2
+var aim_position : Vector3
 
 var mouse_position : Vector2
 var _is_using_mouse : bool = true
@@ -29,21 +30,22 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	if is_using_mouse:
-		var target_position = (source.global_position - self.global_position) * (Vector3.ONE - Vector3.UP)
-		aim_vector = Vector2(target_position.x, target_position.z)
 		self.global_position = mouse_plane.intersects_ray(camera.project_ray_origin(mouse_position), camera.project_ray_normal(mouse_position))
+		aim_position = self.position * (Vector3.ONE - Vector3.UP)
+		aim_vector = Vector2(aim_position.x, aim_position.z)
 	else:
 		aim_vector = Input.get_vector("aim_left", "aim_right", "aim_up", "aim_down").normalized()
 		# mesh.visible = aim_vector != Vector2.ZERO
 		if aim_vector:
-			var target_position := Vector3(aim_vector.x * aim_distance, self.position.y, aim_vector.y * aim_distance)
-			self.position = lerp(self.position, target_position, aim_visual_alpha * delta)
-	mesh.look_at(Vector3(aim_vector.x, 0, aim_vector.y))
+			aim_position = Vector3(aim_vector.x * aim_distance, 0, aim_vector.y * aim_distance)
+		self.position = lerp(self.position, aim_position, aim_visual_alpha * delta)
+	self.global_position.y = 0
+	self.look_at(((self.global_position - self.position) * (Vector3.ONE - Vector3.UP)))
 
 
 func _input(event: InputEvent) -> void:
 	if event is InputEventJoypadButton or event is InputEventJoypadMotion:
 		is_using_mouse = false
-	if event is InputEventMouseMotion:
+	if event is InputEventMouseMotion and Input.mouse_mode == Input.MOUSE_MODE_CONFINED_HIDDEN:
 		is_using_mouse = true
 		mouse_position = event.position
--- assets/scripts/player_input.gd
+++ assets/scripts/player_input.gd
@@ -1,10 +1,16 @@
 
 extends Node
 
-const SPEED = 5.0
 const JUMP_VELOCITY = 4.5
 
+@export var move_speed : float = 5.0
+
 @onready var pawn : CharacterBody3D = self.get_parent()
+
+var move_vector : Vector2
+
+func _process(delta: float) -> void:
+	move_vector = Input.get_vector("move_left", "move_right", "move_up", "move_down").normalized()
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
@@ -20,10 +26,10 @@ func _physics_process(delta: float) -> void:
 	var input_dir := Input.get_vector("move_left", "move_right", "move_up", "move_down")
 	var direction := (pawn.transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	if direction:
-		pawn.velocity.x = direction.x * SPEED
-		pawn.velocity.z = direction.z * SPEED
+		pawn.velocity.x = direction.x * move_speed
+		pawn.velocity.z = direction.z * move_speed
 	else:
-		pawn.velocity.x = move_toward(pawn.velocity.x, 0, SPEED)
-		pawn.velocity.z = move_toward(pawn.velocity.z, 0, SPEED)
+		pawn.velocity.x = move_toward(pawn.velocity.x, 0, move_speed)
+		pawn.velocity.z = move_toward(pawn.velocity.z, 0, move_speed)
 
 	pawn.move_and_slide()

ebc0df03cbadd274573d5280d7f6519ddd5fff61
Liam Wofford <27900656+nulture@users.noreply.github.com>
2025-01-20 07:07:16 -0600

    Changed pawn movement to use orientation vector

--- assets/scripts/player_movement.gd
+++ assets/scripts/player_movement.gd
@@ -7,6 +7,8 @@ const JUMP_VELOCITY = 4.5
 
 @export var walk_speed : float = 5.0
 @export var walk_damping : float = 5.0
+@export var turn_speed : float = 1.0
+@export_range(0.0, 1.0) var turn_limit : float = 0.8
 
 @export_category("Dodge")
 
@@ -16,20 +18,30 @@ const JUMP_VELOCITY = 4.5
 
 @onready var pawn : CharacterBody3D = self.get_parent()
 
-var move_vector : Vector3
+var input_vector : Vector2
+var move_vector : Vector3 :
+	get: return Vector3(input_vector.x, 0, input_vector.y)
+var orientation_vector : Vector2
+
 var is_dodging : bool
 
 var damping : float :
 	get: return dodge_damping if is_dodging else walk_damping
 
 func _process(delta: float) -> void:
-	var input_vector := Input.get_vector("move_left", "move_right", "move_up", "move_down").normalized()
-	move_vector = Vector3(input_vector.x, 0, input_vector.y)
+	input_vector = Input.get_vector("move_left", "move_right", "move_up", "move_down").normalized()
+	orientation_vector = Vector2(move_vector.dot(pawn.global_basis.x), move_vector.dot(pawn.global_basis.z))
+
+	if input_vector and not is_dodging:
+		var turn_amount := signf(orientation_vector.x) if orientation_vector.y >= 0.0 else orientation_vector.x
+		pawn.rotate_y(turn_amount * turn_speed * delta * -1.0)
+		# pawn.rotate_y(sqrt(abs(turn_amount)) * signf(turn_amount) * turn_speed * delta * -1.0)
+
 
 func _physics_process(delta: float) -> void:
-	var move_direction := (pawn.transform.basis * move_vector)
+	var move_direction := -pawn.transform.basis.z
 
-	if not is_dodging:
+	if input_vector and not is_dodging and orientation_vector.y < -turn_limit:
 		pawn.velocity += move_direction * walk_speed * delta
 
 	pawn.velocity -= pawn.velocity * damping * delta
@@ -45,6 +57,6 @@ func _input(event: InputEvent) -> void:
 func dodge() -> void:
 	if is_dodging: return
 	is_dodging = true
-	pawn.velocity += move_vector * dodge_impulse
+	pawn.velocity += (move_vector if move_vector else -pawn.global_basis.z) * dodge_impulse
 	await get_tree().create_timer(dodge_delay).timeout
 	is_dodging = false

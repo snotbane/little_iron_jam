2fc274bfd4518983c96d74e631aae92b730aacd7
Liam Wofford <27900656+nulture@users.noreply.github.com>
2025-01-20 06:06:10 -0600

    Added proper vacuuming

--- assets/scripts/vacuum.gd
+++ assets/scripts/vacuum.gd
@@ -1,10 +1,15 @@
 
 class_name Vacuum extends Area3D
 
-@onready var region : Camera3D = $region
+@export var region : Camera3D
+@export var suck_angle : float = 45.0 :
+	get: return region.fov
+	set(value):
+		if region.fov == value: return
+		region.fov = value
 
-@export var suck_angle := 30.0
 @export var suck_power := 1.0
+@export var collect_radius_squared := 1.0
 
 var prospects : Array[RigidBody3D]
 
@@ -25,23 +30,24 @@ func _ready() -> void:
 func _physics_process(delta: float) -> void:
 	if is_sucking:
 		for i in prospects.size():
-			try_collect(prospects.pop_front())
+			var body : RigidBody3D = prospects.pop_front()
+
+			if body.find_child("safe"): continue
+
+			var difference := self.global_position - body.global_position
+			if difference.length_squared() < collect_radius_squared:
+				collect(body); continue
+			elif region.is_position_in_frustum(body.global_position * Vector3(1, 0, 1)):
+				body.apply_force(difference.normalized() * suck_power)
+			fail_collect(body)
 
 
 func _body_entered(body: Node3D) -> void:
-	try_collect(body)
+	prospects.push_back(body)
 
 
 func _body_exited(body: Node3D) -> void:
 	prospects.erase(body)
-
-
-func try_collect(body: RigidBody3D) -> void:
-	print(body)
-	if is_sucking and not body.find_child("safe"):
-		collect(body)
-	else:
-		fail_collect(body)
 
 
 func collect(body: RigidBody3D) -> void:

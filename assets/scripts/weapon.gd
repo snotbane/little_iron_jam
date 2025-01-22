197a604aabe56ad71e166a8078d53577492e2340
Liam Wofford <27900656+nulture@users.noreply.github.com>
2025-01-21 23:04:40 -0600

    Created pistol fire volley

--- assets/scripts/weapon.gd
+++ assets/scripts/weapon.gd
@@ -28,6 +28,6 @@ func fire(ammo: Ammo) -> void:
 func create_bullet(shooter: Node3D) -> void:
 	var projectile : Bullet = bullet_scene.instantiate()
 	get_tree().root.add_child(projectile)
-	projectile.global_rotation = self.global_rotation
+	projectile.global_rotation = bullet_spawn_location.global_rotation
 	projectile.global_position = bullet_spawn_location.global_position + projectile.basis.z * 1.5
 	projectile.populate(shooter)
--- assets/scripts/weapon_config_pistol.gd
+++ assets/scripts/weapon_config_pistol.gd
@@ -17,5 +17,6 @@ func _set_is_shooting(value: bool) -> void:
 
 func fire_volley() -> void:
 	for weapon in visible_weapons:
+		await get_tree().create_timer(randf_range(0.02, 0.05)).timeout
 		weapon.fire(ammo)
 

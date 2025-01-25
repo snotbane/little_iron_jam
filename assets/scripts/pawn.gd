35bc4f7c9f2634930185ac9066b9c7e6e84d1b7c
Liam Wofford <27900656+nulture@users.noreply.github.com>
2025-01-25 00:25:33 -0600

    Added some wave logic

--- assets/scripts/pawn.gd
+++ assets/scripts/pawn.gd
@@ -4,3 +4,8 @@ extends Node3D
 signal received_weapon_config(weapon_config: WeaponConfig)
 
 @export var damage := 1
+
+
+func _ready() -> void:
+	if self.is_in_group(&"enemy"):
+		tree_exited.connect(WaveController.inst.check_enemy_group)

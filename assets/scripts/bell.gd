a66003e766f5400724174e25a3b5d7c55c2376c7
Liam Wofford <27900656+nulture@users.noreply.github.com>
2025-01-25 18:00:03 -0600

    Added bell, and sounds, and wilhelm

---
+++ assets/scripts/bell.gd
@@ -0,0 +1,28 @@
+
+class_name Bell extends AnimatableBody3D
+
+signal bell_rang
+
+
+@export var anim_player : AnimationPlayer
+
+
+var _is_enabled : bool
+var is_enabled : bool :
+	get: return _is_enabled
+	set(value):
+		if _is_enabled == value: return
+		_is_enabled = value
+
+		anim_player.stop()
+		anim_player.play(&"popup" if _is_enabled else &"ring")
+
+
+func _ready() -> void:
+	anim_player.play(&"ring", -1, 1, true)
+
+
+func on_hit(body: Node3D) -> void:
+	if is_enabled:
+		is_enabled = false
+		bell_rang.emit()
--- assets/scripts/casing_spawner.gd
+++ assets/scripts/casing_spawner.gd
@@ -1,8 +1,14 @@
 extends Node3D
 
 @export var casing_scene : PackedScene = preload("res://assets/scenes/detritus/detritus_casing.tscn")
+@export var count := 1
+
 
 func spawn() -> void:
+	for i in count:	spawn_single()
+
+
+func spawn_single() -> void:
 	var casing : Detritus = casing_scene.instantiate()
 	get_tree().root.add_child(casing)
 	casing.global_position = self.global_position

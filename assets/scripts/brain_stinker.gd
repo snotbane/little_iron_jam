523c49427f4af1b25f12f5ee49449c85419f61ba
Liam Wofford <27900656+nulture@users.noreply.github.com>
2025-01-25 23:41:52 -0600

    Implemented root motion

--- assets/scripts/brain_stinker.gd
+++ assets/scripts/brain_stinker.gd
@@ -2,45 +2,59 @@
 extends Brain
 
 enum State {
+	IDLING,
 	CHASING,
-	IDLE,
-	ATTACK,
+	ATTACKING,
 }
 
-@export var lunge_pre_delay : float = 0.25
-@export var lunge_duration : float = 0.25
-@export var lunge_post_delay : float = 0.5
-@export var lunge_speed : float = 100.0
-
 var _state : State
-var state : State :
+@export var state : State :
 	get: return _state
 	set(value):
 		if _state == value: return
 		_state = value
 
-		pawn.set_collision_layer_value(3, _state == State.ATTACK)
+
+var is_walking : bool :
+	get: return pawn.velocity.length_squared() > 0.1
+
+
+var is_attacking_blip : bool :
+	get: return state == State.ATTACKING
+
+
+var _is_volatile : bool
+@export var is_volatile : bool :
+	get: return _is_volatile
+	set(value):
+		if _is_volatile == value: return
+		_is_volatile = value
+
+		pawn.set_collision_layer_value(3, _is_volatile)
+func set_is_volatile(value: bool) -> void:
+	is_volatile = value
 
 
 func _ready() -> void:
 	super._ready()
+	await get_tree().create_timer(1.0).timeout
+
+	state = State.CHASING
 	self.target_reached.connect(attack)
+
 
 func _physics_process(delta: float) -> void:
 	if not target: return
 	match state:
 		State.CHASING:
 			physics_process_walk_to_target(delta)
-		State.ATTACK:
-			pawn.velocity = -pawn.global_basis.z * lunge_speed * delta
-			pawn.move_and_slide()
+		_:
+			pawn.velocity = Vector3.ZERO
 
 
 func attack() -> void:
-	state = State.IDLE
-	await wait(lunge_pre_delay)
-	state = State.ATTACK
-	await wait(lunge_duration)
-	state = State.IDLE
-	await wait(lunge_post_delay)
+	state = State.ATTACKING
+	await get_tree().create_timer(0.5).timeout
+	state = State.IDLING
+	await get_tree().create_timer(2.0).timeout
 	state = State.CHASING
